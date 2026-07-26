6d22f9ba8df3f21ceb535d6ae9d736d4c9af0b43
Nuxffone <nuxffone@github.com>
2026-07-26 23:40:30 +0800

    fix: Identifier.of -> fromNamespaceAndPath, remove ModMenu dep

--- src/main/java/net/fabricmc/crossbowaim/config/ModMenuIntegration.java
+++ src/main/java/net/fabricmc/crossbowaim/config/ModMenuIntegration.java
@@ -1,4 +1,1 @@
-package net.fabricmc.crossbowaim.config;
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-public class ModMenuIntegration implements ModMenuApi{@Override public ConfigScreenFactory<?> getModConfigScreenFactory(){return parent->null;}}
+package net.fabricmc.crossbowaim.config;public class ModMenuIntegration{}
